4dd43aaf027bead4caec9048fc8d671a8f4135a4
abdullah.ra2345@gmail.com <rubys@intertwingly.net>
2024-02-08 18:15:51 +0500

    practice on text area validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
-  validates :title, :description, presence: true
+  validates :description, length: { maximum: 1500, message: "Maximum word count exceeded. Please limit your input to 1500 words." }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
-  validates :image_url, presence: true
+  validates :image_url, allow_blank: true, format: { with:
+                                                       %r{\.(gif|jpg|png)\z}i,
+                                                     message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 =begin
 
   validate :image_file_format
